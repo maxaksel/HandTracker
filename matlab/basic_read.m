43a25f7579e172dc972fc079e325847f690600b2
Max <maxabowman@gmail.com>
2022-04-29 12:34:57 -0500

    Matlab sensor fusion update.

--- matlab/basic_read.m
+++ matlab/basic_read.m
@@ -14,27 +14,34 @@ while ~isequal(alignment_buffer, [12*16+10 15*16+14])
 end
 
 disp("Aligned.");
-
+buffer = read(device, 9, 'int16'); % maintain alignment
 % Read data from glove
 while 1
     buffer = read(device, 10, 'int16');
+    % disp(buffer(1));
+    gyro = buffer(5:7) * 2000 / (2^15); % DPS
+    acc = buffer(2:4) * 2 * 9.8 / (2^15); % g-forces
     % disp(buffer);
-    gyro = buffer(5:7) * 2000 / (2^16); % DPS
-    acc = buffer(2:4) * 4 / (2^16); % g-forces
-    % disp(buffer);
-    % disp(buffer);
-    res = [0 0 0 0 0];
-    res(1) = bitand(int16(buffer(8)), int16(hex2dec('FF00')), 'int16');
-    res(1) = bitshift(res(1), -8, 'int16'); % shift upper bits to lower place
-    res(2) = bitand(int16(buffer(8)), int16(hex2dec('00FF')), 'int16');
-    res(3) = bitand(int16(buffer(9)), int16(hex2dec('FF00')), 'int16');
-    res(3) = bitshift(res(3), -8, 'int16'); % shift upper bits to lower place
-    res(4) = bitand(int16(buffer(9)), int16(hex2dec('00FF')), 'int16');
-    res(5) = bitand(int16(buffer(10)), int16(hex2dec('FF00')), 'int16');
-    res(5) = bitshift(res(5), -8, 'int16'); % shift upper bits to lower place
-    inputs = bitand(int16(buffer(10)), int16(hex2dec('00FF')), 'int16');
     
-    disp(acc);
+    res = [0 0 0 0 0];
+    res(1) = bitand(int16(buffer(8)), int16(hex2dec('00FF')), 'int16');
+    
+    res(2) = bitand(typecast(int16(buffer(8)), "uint16"), uint16(hex2dec('FF00')), 'uint16');
+    res(2) = bitshift(uint16(res(2)), -8, 'uint16'); % shift upper bits to lower place
+    
+    res(3) = bitand(int16(buffer(9)), int16(hex2dec('00FF')), 'int16');
+    
+    res(4) = bitand(typecast(int16(buffer(9)), "uint16"), uint16(hex2dec('FF00')), 'uint16');
+    res(4) = bitshift(uint16(res(4)), -8, 'uint16'); % shift upper bits to lower place
+    
+    res(5) = bitand(int16(buffer(10)), int16(hex2dec('00FF')), 'int16');
+    
+    inputs = bitand(int16(buffer(10)), int16(hex2dec('FF00')), 'int16');
+    inputs = bitshift(int16(inputs), -8, 'int16'); % shift upper bits to lower place
+    
+    % disp(acc);
+    % disp(gyro);
+    % disp(res);
     % fprintf("Gyro: %f, %f, %f | Acc: %f, %f, %f | Res: %d, %d, %d, %d, %d | Input: %d\n",...
     % gyro(1), gyro(2), gyro(3), acc(1), acc(2), acc(3), res(1), res(2), res(3), res(4), res(5), inputs);
 end
--- matlab/sensor_fusion.m
+++ matlab/sensor_fusion.m
@@ -1,12 +1,12 @@
 % Script for debugging orientation tracking.
 % 
-% @date 04/28/2022
+% @date 04/29/2022
 clear;clc;
 baud_rate = 38400;
-imu_sample_rate = 40;
+imu_sample_rate = 50; % 50 Hz
 device = serialport("COM7", baud_rate);
 
-imu_filter = imufilter('SampleRate', imu_sample_rate);
+FUSE = imufilter('SampleRate', imu_sample_rate);
 
 % Align with incoming data
 alignment_buffer = [0 0];
@@ -15,27 +15,36 @@ while ~isequal(alignment_buffer, [12*16+10 15*16+14])
     alignment_buffer = [alignment_buffer(2) in];
     disp(alignment_buffer);
 end
-
 disp("Aligned.");
+read(device, 9, "int16");
 
-% Read data from glove
 angles = [0 0 0];
 update = 0;
-update_period = 250;
-acc_buffer = zeros(250, 3);
-gyro_buffer = zeros(250, 3);
+update_period = 10;
+acc_buffer = zeros(update_period, 3);
+gyro_buffer = zeros(update_period, 3);
+
+% update_timer = timer('ExecutionMode', 'fixedRate', 'Period', 1/imu_sample_rate, 'TimerFcn', @update_orientation);
+% start(update_timer);
+
+% Uses global variables: update, update_period, acc_buffer, and gyro_buffer
 while 1
-    if update == 250
-        [orientation, omega] = FUSE(acc_buffer, gyro_buffer);
-        angles = angles + eulerd(orientation, 'ZYX', 'frame');
+    tic
+    % Read data from glove
+    if update == update_period
+        [orientation, ~] = FUSE(acc_buffer, gyro_buffer);
+        angles = eulerd(orientation, 'ZYX', 'frame');
+        angles = mod(angles, 360);
         fprintf("X angle: %f, Y angle: %f, Z angle: %f\n", angles(3), angles(2), angles(1));
         update = 0;
+        % disp(angles);
     end
-    
+
     buffer = read(device, 10, 'int16');
-    gyro = buffer(5:7) * 2000 / (2^16); % DPS
-    acc = buffer(2:4) * 4 / (2^16); % g-forces
-    acc_buffer(update) = gyro';
-    gyro_buffer(update) = acc';
+    gyro = buffer(5:7) * (2000 / (2^15)) * (pi / 180); % rad/s
+    acc = buffer(2:4) * 2 * 9.81 / (2^15); % g-forces
+    acc_buffer(update + 1, :) = gyro;
+    gyro_buffer(update + 1, :) = acc;
     update = update + 1;
+    pause(1/50 - toc)
 end
